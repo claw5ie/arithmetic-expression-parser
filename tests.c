508acfe914ccf4e3605d6a17cc8711f31b0e49a8
claw5ie <92851066+claw5ie@users.noreply.github.com>
2022-09-22 10:57:16 +0100

    Refactor tests a little bit

--- tests.c
+++ tests.c
@@ -1,6 +1,7 @@
 #include "parser.c"
 
 #include <string.h>
+#include <stddef.h>
 
 typedef struct
 {
@@ -9,58 +10,49 @@ typedef struct
 } Test;
 
 Test tests[]
-= { {       234, "234" },
-    {        10, "2+8" },
-    {        -4, "1-2-3" },
-    {         6, "2+2*2" },
-    {         8, "(2+2)*2" },
-    {         6, "2*2+2" },
-    {         8, "2*(2+2)" },
-    { 134217728, "2^3^3" },
-    {      4096, "(2^3)^4" },
-    {       -75, "2^2-3^4+2^-3+2" },
-    {         1, "--2+-3+-(-(2))" },
-    {       -90, "2+2*2-2^3*3*(2+2)" } };
-
+  = { {       234, "234" },
+      {        10, "2+8" },
+      {        -4, "1-2-3" },
+      {         6, "2+2*2" },
+      {         8, "(2+2)*2" },
+      {         6, "2*2+2" },
+      {         8, "2*(2+2)" },
+      { 134217728, "2^3^3" },
+      {      4096, "(2^3)^4" },
+      {       -75, "2^2-3^4+2^-3+2" },
+      {         1, "--2+-3+-(-(2))" },
+      {       -90, "2+2*2-2^3*3*(2+2)" } };
 
 void
 assert_equals(Test test)
 {
-  printf("* running test...");
+  printf("* Running test...");
 
   at = test.expr;
   int actual = parse_expr();
 
-  int expected_len = strlen(test.expr);
-  int actual_len = at - test.expr;
+  ptrdiff_t expected_length = strlen(test.expr);
+  ptrdiff_t actual_length = at - test.expr;
 
-  if (actual == test.expected)
-    {
-      if (*at != '\0' || expected_len != actual_len)
-        goto report_error;
-      else
-        printf(" Ok.\n");
-    }
+  if (actual == test.expected && expected_length == actual_length)
+    puts(" Ok.");
   else
-    goto report_error;
+    {
+      printf(" Failed."
+             "\n  - Expected:   %i"
+             "\n  - Actual:     %i"
+             "\n  - Expression: \'%s\'\n",
+             test.expected,
+             actual,
+             test.expr);
 
-  return;
+      actual_length += 17;
 
- report_error:
-  printf(" Failed."
-         "\n  - Expected:   %i"
-         "\n  - Actual:     %i"
-         "\n  - Expression: \'%s\'\n",
-         test.expected,
-         actual,
-         test.expr);
+      while (actual_length-- > 0)
+        putchar(' ');
 
-  actual_len += 17;
-
-  while (actual_len-- > 0)
-    putchar(' ');
-
-  puts("^");
+      puts("^");
+    }
 }
 
 int
